5bfdbebf4af3ee1c8cfec600c3d33f0d49b3e00f
Mathias Pettersen <mathias.pettersen@proton.me>
2024-10-28 15:16:14 +0100

    Fixed follow-file config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,31 +2,33 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-      follow_current_file = true,
-      use_libuv_file_watcher = true,
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+			},
+			follow_current_file = {
+				enabled = true,
+			},
+			use_libuv_file_watcher = true,
+		},
+	},
 }
